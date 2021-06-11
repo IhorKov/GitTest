769ceda96067f97c5b09f3b80ab54b0929fe06d3
IhorKov <apasniy1234@gmail.com>
2021-06-11 15:03:43 +0300

    add an address

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,14 @@ public class Main {
     public static void main(String[] args) {
         int age;
         double salary;
-        String name, position;
+        String name, position, address;
 
         age = 26;
         salary = safdas;
+        address = "Kharkov";
 
         System.out.println(age);
         System.out.println(salary);
+        System.out.println(address);
     }
 }
